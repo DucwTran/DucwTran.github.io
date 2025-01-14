8980be08d2d609eee3432d480f72c51bea46d018
Tran Cong Duc <trancongduc0812@gmail.com>
2025-01-14 23:13:15 +0700

    render pokemon base

---
+++ index.js
@@ -0,0 +1,19 @@
+let htmls = "";
+pokemons.forEach((pokemon, index) =>{
+    htmls += `
+            <div class="item">
+                <div class="item__id">
+                    #${index + 1}
+                </div>
+                <div class="item__image" style="background-image: url('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png');">
+                    
+                </div>
+                <div class="item__name">
+                    ${pokemon.name}
+                </div>
+            </div>
+        `
+});
+
+const itemPokemon = document.querySelector('.items');
+itemPokemon.innerHTML = htmls;
